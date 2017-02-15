226b45eab151d8c211dff4044b5be5dc986fe108
Daniel Kocielinski <dkocielinski@gmail.com>
2017-02-16 00:43:45 +0100

    fixup! CAFE1: Create BaristaGetter

--- cafe_params.cpp
+++ cafe_params.cpp
@@ -4,4 +4,12 @@ namespace cafe {
 
 const char* BARISTA_NAMES_PATH = "..\\data\\barista_names.txt";
 
+const char* DRINKS_PATH = "..\\data\\drinks.txt";
+
+const char* ADD_ONS_PATH = "..\\data\\add-ons.txt";
+
+const char* ORDERS_PATH = "..\\data\\orders.txt";
+
+const char* RECEIPTS_PATH = "receipts.txt";
+
 }  // namespace cafe
--- cafe_params.h
+++ cafe_params.h
@@ -5,4 +5,24 @@ namespace cafe {
  */
 extern const char* BARISTA_NAMES_PATH;
 
+/**
+ * Relative path to the text file containing drink's menu.
+ */
+extern const char* DRINKS_PATH;
+
+/**
+ * Relative path to the text file containing add-ons menu.
+ */
+extern const char* ADD_ONS_PATH;
+
+/**
+ * Relative path to the text file containing orders.
+ */
+extern const char* ORDERS_PATH;
+
+/**
+ * Relative path to the text file for the generated receipts.
+ */
+extern const char* RECEIPTS_PATH;
+
 }  // namespace cafe
